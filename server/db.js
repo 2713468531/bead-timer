335aae5946cbba29bce8d1465860383d7180e9cd
Deploy User <deploy@example.com>
2026-06-06 15:09:23 +0800

    修复：添加更多字符集设置

--- server/db.js
+++ server/db.js
@@ -13,7 +13,7 @@ const dbConfig = {
   charset: 'utf8mb4',
   timezone: '+00:00',
   collation: 'utf8mb4_unicode_ci',
-  initSqls: ['SET NAMES utf8mb4', 'SET CHARACTER SET utf8mb4']
+  initSqls: ['SET NAMES utf8mb4', 'SET CHARACTER SET utf8mb4', 'SET character_set_client = utf8mb4', 'SET character_set_results = utf8mb4', 'SET character_set_connection = utf8mb4']
 }
 
 // 创建连接池
